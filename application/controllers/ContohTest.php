44385413c9d1600be4147f4af662fd21e0af33c6
ferdianexe <ferdianboy@hotmail.com>
2019-01-31 14:17:57 +0700

    add test via terminal command

--- application/controllers/ContohTest.php
+++ application/controllers/ContohTest.php
@@ -1,10 +1,11 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+// php index.php ContohTest
 class ContohTest extends CI_Controller {
         public function __construct()
         {
             parent::__construct(); 
-			$this->load->library("unit_test");	
+			$this->load->library("unit_test");
         }
 		
 		
@@ -13,10 +14,20 @@ class ContohTest extends CI_Controller {
 		}
 		
 		public function index(){
-			echo "Using Unit Test Library";	
 			$test = $this->division(6,3);
 			$expected_result = 2;
 			$test_name = "Division test function";
-			echo $this->unit->run($test,$expected_result,$test_name);
-		}
+			$this->unit->run($test,$expected_result,$test_name);
+			$results = $this->unit->result();
+	        foreach ($results as $result) {
+			echo "=== " . $result['Test Name'] . " ===\n";
+            foreach ($result as $key => $value) {
+				echo "$key: $value\n";
+				if($key ==='Result'){
+					break;
+				}
+            }
+            echo "\n";
+        }
+	}
 }		

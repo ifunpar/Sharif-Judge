7c1616090f2cdfefdae3bbffa9c00d26ece9a53d
jy006 <yehezkieljason@gmail.com>
2019-03-05 16:34:19 +0700

    Add and complete Setting model test coverage (#22)
    * add and complete Setting model test coverage
    
    * remove unnecessary model load
    
    * clean code, remove comments

--- application/models/tests/Settingtest_model.php
+++ application/models/tests/Settingtest_model.php
@@ -12,49 +12,67 @@ class Settingtest_model extends Test_model
 	public function __construct()
 	{
         parent::__construct();
-        $this->load->model("Notifications_model");
-        $this->load->model("User_model");
+        $this->load->model("Settings_model");
 	}
 
     // ------------------------------------------------------------------------
     
     public function test(){
-        $this->printSetting();
+        $prevSetting = $this->settings_model->get_all_settings();
 
-        $set = $this->set_setting("timezone", "Asia/Tokyo");
-        $test = $this->get_setting("timezone");
+        $this->individualSettingTest();
+        $this->setAllSettingTest();
+
+        $this->settings_model->set_settings($prevSetting);
+    }
+
+    public function individualSettingTest() {
+        $set = $this->settings_model->set_setting("timezone", "Asia/Tokyo");
+        $test = $this->settings_model->get_setting("timezone");
         $expected_result = "Asia/Tokyo";
-        $test_name = "Change Time Zone test function";
-
-        $this->unit->run($test,$expected_result,$test_name);
-    }
-
-    public function printSetting(){
-        $testAll = $this->get_all_settings();
-        echo "Current Setting";
-        // print_r($testAll);
-        echo "\n";
-    }
-    
-    public function set_setting($key, $value)
-    {
-        $this->db->where('shj_key', $key)->update('settings', array('shj_value'=>$value));
-    }
-
-    public function get_setting($key)
-    {
-        return $this->db->select('shj_value')->get_where('settings', array('shj_key'=>$key))->row()->shj_value;
-    }
-
-    public function get_all_settings()
-    {
-        $result = $this->db->get('settings')->result_array();
-        $settings = array();
-        foreach($result as $item)
-        {
-            $settings[$item['shj_key']] = $item['shj_value'];
+        $testString = $test;
+        $expected_resultString = $expected_result;
+        if($testString == NULL){
+            $testString = 'NULL';
         }
-        return $settings;
+        if($expected_resultString == NULL){
+            $expected_resultString = 'NULL';
+        }
+        $notes = "Test: $testString <br>" .
+                "Expected result: $expected_resultString<br>" .
+                "Last test date: " . date('H:i:s ~ Y-m-d');
+        $test_name = "Change Time Zone (Setting) || individual set setting test function";
+
+        $this->unit->run($test,$expected_result,$test_name,$notes);
+    }
+
+    public function setAllSettingTest() {
+        $expected_result = array(
+            'timezone' => 'Asia/Jakarta',
+            'mail_from' => 'test_email@email.com',
+            'mail_from_name' => 'One lonely test mail account'
+        );
+        $this->settings_model->set_settings($expected_result);
+        $test = $this->settings_model->get_all_settings();
+        $test = array(
+            'timezone' => $test['timezone'],
+            'mail_from' => $test['mail_from'],
+            'mail_from_name' => $test['mail_from_name']
+        );
+        $testString = $test;
+        $expected_resultString = $expected_result;
+        if($testString == NULL){
+            $testString = 'NULL';
+        }
+        if($expected_resultString == NULL){
+            $expected_resultString = 'NULL';
+        }
+        $notes = "Test: $testString <br>" .
+                "Expected result: $expected_resultString<br>" .
+                "Last test date: " . date('H:i:s ~ Y-m-d');
+        $test_name = "Change Setting || set all setting test function";
+
+        $this->unit->run($test,$expected_result,$test_name,$notes);
     }
 
 	// ------------------------------------------------------------------------

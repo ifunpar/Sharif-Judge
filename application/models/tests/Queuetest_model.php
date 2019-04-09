bede8b39feee44199fc8753c27f64863043b9082
Hereza Liem <herezaardhitya@gmail.com>
2019-04-09 16:44:23 +0900

    Add Queue Test

--- application/models/tests/Queuetest_model.php
+++ application/models/tests/Queuetest_model.php
@@ -191,15 +191,15 @@ class Queuetest_model extends Test_model
         $dummyAssignment =
             array(
                 'name' => 'Test rejudge single',
-                'problems' => '1',
-                'total_submits' => '0',
-                'open' => '0',
-                'scoreboard' => '0',
-                'javaexceptions' => '0',
+                'problems' => 1,
+                'total_submits' => 0,
+                'open' => 0,
+                'scoreboard' => 0,
+                'javaexceptions' => 0,
                 'description' => '',
                 'start_time' => '2019-02-14 00:00:00',
                 'finish_time' => '2019-03-01 00:00:00',
-                'extra_time' => '0',
+                'extra_time' => 0,
                 'late_rule' => '/* 
           * Put coefficient (from 100) in variable $coefficient.
           * You can use variables $extra_time and $delay.
@@ -245,14 +245,91 @@ class Queuetest_model extends Test_model
             'main_file_name' => 'Main',
             'file_type' => 'java'
         );
-        $this->db->insert('submissions',$dummyArray);
         $this->db->insert('problems', $dummyArray1);
         $this->db->insert('assignment', $dummyAssignment);
-        $test = $this->Queue_model->rejudge_single(1);
+        $this->db->insert('submissions',$dummyArray);
+        $test = $this->Queue_model->rejudge_single($dummyArray);
         $expected_result = 0;
         $this->unit->run($test,$expected_result,$test_name);
+        $this->emptyDB('problems');
+        $this->emptyDB('assignment');
+        $this->emptyDB('submissions');
 
-        $test1 = $this->Queue_model->rejudge_single(1);
+        $dummyArray2 = array(
+            'assignment'=> 1,
+            'id' => 1,
+            'name' => "asdf.exe",
+            'score'=> 100,
+            'is_upload_only' => 0,
+            'c_time_limit' => 2000,
+            'python_time_limit' => 2000,
+            'java_time_limit' => 2000,
+            'memory_limit' => 128,
+            'allowed_languages' => 'java',
+            'diff_cmd' => 'diff',
+            'diff_arg' => '-bB'
+        );
+        $dummyAssignment2 =
+            array(
+                'name' => 'Test rejudge single',
+                'problems' => 1,
+                'total_submits' => 0,
+                'open' => 0,
+                'scoreboard' => 0,
+                'javaexceptions' => 0,
+                'description' => '',
+                'start_time' => '2019-02-14 00:00:00',
+                'finish_time' => '2019-03-01 00:00:00',
+                'extra_time' => 0,
+                'late_rule' => '/* 
+          * Put coefficient (from 100) in variable $coefficient.
+          * You can use variables $extra_time and $delay.
+          * $extra_time is the total extra time given to users
+          * (in seconds) and $delay is number of seconds passed
+          * from finish time (can be negative).
+          *  In this example, $extra_time is 172800 (2 days):
+          */
+          if ($delay<=0)
+            // no delay
+            $coefficient = 100;
+          
+          elseif ($delay<=3600)
+            // delay less than 1 hour
+            $coefficient = ceil(100-((30*$delay)/3600));
+          
+          elseif ($delay<=86400)
+            // delay more than 1 hour and less than 1 day
+            $coefficient = 70;
+          
+          elseif (($delay-86400)<=3600)
+            // delay less than 1 hour in second day
+            $coefficient = ceil(70-((20*($delay-86400))/3600));
+          
+          elseif (($delay-86400)<=86400)
+            // delay more than 1 hour in second day
+            $coefficient = 50;
+          
+          elseif ($delay > $extra_time)
+            // too late
+            $coefficient = 0;','participants' => 'ALL','moss_update' => 'Never','archived_assignment' => '0');
+        $dummyArrays = array(
+            'submit_id' => 1,
+            'username' => "Aku Tester",
+            'assignment'=> 1,
+            'problem' => 1,
+            'is_final' => 1,
+            'status' => '',
+            'time' => "2019-02-13 00:00:00",
+            'pre_score' => 100,
+            'coefficient' => 25,
+            'file_name' => 'Main',
+            'main_file_name' => 'Main',
+            'file_type' => 'java'
+        );
+        $this->db->insert('problems', $dummyArray2);
+        $this->db->insert('assignment', $dummyAssignment2);
+        $this->db->insert('submissions',$dummyArrays);
+        $test1 = $this->Queue_model->rejudge_single($dummyArrays);
         $expected_result1 = 0;
         $this->unit->run($test1,$expected_result1,$test_name);
 

f046801421767d17ba68c93e49e8347ecf3f2ab9
aeowlian <yehezkieljason@gmail.com>
2019-04-09 16:26:09 +0700

    complete test

--- application/models/tests/Assignmenttest_model.php
+++ application/models/tests/Assignmenttest_model.php
@@ -20,19 +20,11 @@ class Assignmenttest_model extends Test_model
 
   public function test()
   {
-    // $this->addDummyAssignment();
-    // //
-    // $this->testing_method_add_assignment();
-    // //
-    // $this->testing_method_all_assignments();
-    // //
-    // $this->testing_method_new_assignment_id();
-    // //
-    // $this->clear();
+    $this->clear();
     $this->initial_test();
 
     //add new assignment (id 1)
-    // $this->testing_method_add_assignment();
+    $this->testing_method_add_assignment();
 
     //add 2 dummy assignment (id 2 and 3)
     $this->addDummyAssignment();
@@ -41,7 +33,7 @@ class Assignmenttest_model extends Test_model
     $this->testing_method_all_assignments();
 
     //delete the first dummy (id 2)
-    $this->assignment_model->delete_assignment(2);
+    $this->test_delete_assignment();
 
     //test new_assignment_id, expect id 2
     $this->testing_method_new_assignment_id();
@@ -50,10 +42,14 @@ class Assignmenttest_model extends Test_model
     $this->test_edit_assignment();
 
     //the rest tbd
-    $this->addDummyAssignment();
+    // $this->addDummyAssignment();
     $this->testing_method_increase_total_submits();
     $this->testing_method_get_moss_time();
     $this->testing_method_set_moss_time();
+
+    $this->test_get_assignment_and_problem_info();
+    $this->test_is_participant();
+    $this->test_save_problem_description();
     $this->clear();
   }
 
@@ -61,6 +57,7 @@ class Assignmenttest_model extends Test_model
   {
     $this->db->truncate('shj_assignments');
     $this->db->truncate('shj_problems');
+    $this->db->truncate('shj_submissions');
   }
 
   private function testing_method_increase_total_submits(){
@@ -107,7 +104,7 @@ class Assignmenttest_model extends Test_model
     $_POST['start_time'] = date('Y-m-d H:i:s', strtotime('04/1/2019 00:00:00'));
     $_POST['finish_time'] = date('Y-m-d H:i:s', strtotime('04/1/2019 12:00:00'));
     $_POST['extra_time'] = 0;
-    $_POST['late_rule'] = "/* 
+    $_POST['late_rule'] = '/* 
                             * Put coefficient (from 100) in variable $coefficient.
                             * You can use variables $extra_time and $delay.
                             * $extra_time is the total extra time given to users
@@ -115,30 +112,10 @@ class Assignmenttest_model extends Test_model
                             * from finish time (can be negative).
                             *  In this example, $extra_time is 172800 (2 days):
                             */
-                          
-                          if ($delay<=0)
-                            // no delay
-                            $coefficient = 100;
-                          
-                          elseif ($delay<=3600)
-                            // delay less than 1 hour
-                            $coefficient = ceil(100-((30*$delay)/3600));
-                          
-                          elseif ($delay<=86400)
-                            // delay more than 1 hour and less than 1 day
-                            $coefficient = 70;
-                          
-                          elseif (($delay-86400)<=3600)
-                            // delay less than 1 hour in second day
-                            $coefficient = ceil(70-((20*($delay-86400))/3600));
-                          
-                          elseif (($delay-86400)<=86400)
-                            // delay more than 1 hour in second day
-                            $coefficient = 50;
-                          
-                          elseif ($delay > $extra_time)
-                            // too late
-                            $coefficient = 0;";
+                            
+                            if (true)
+                              // no delay
+                              $coefficient = 100;';
     $_POST['participants'] = 'ALL';
     $_POST['archived_assignment'] = 0;
 
@@ -169,6 +146,8 @@ class Assignmenttest_model extends Test_model
     $_POST['assignment_name'] = $expected_result;
     $_POST['is_upload_only'] = null;
     $_POST['languages'] = array('C,C++,python2,python3,Java,pdf', '');
+    $this->add_dummy_submission();
+    $new_id = $this->assignment_model->new_assignment_id();
     $this->Assignment_model->add_assignment(3, true);
     $test = $this->db->where('id', 3)->get('assignments')->result_array();
     $test = $test[0]['name'];
@@ -357,5 +336,61 @@ class Assignmenttest_model extends Test_model
     $this->db->insert('shj_assignments', $shj_assignmentsdummy[1]);
   }
 
+  private function test_get_assignment_and_problem_info(){
+    $test = $this->Assignment_model->assignment_info(4);
+    $expected_result = array(
+                              'id' => 0,
+                              'name' => "Not Selected",
+                              'finish_time' => 0,
+                              'extra_time' => 0,
+                              'problems' => 0
+                            );
+    $test_name = "Test get assignment info || input : - \nTime ~ Date: " . date('H:i:s ~ Y-m-d');
+    $this->unit->run($test, $expected_result, $test_name);
 
+    $test = $this->Assignment_model->problem_info(1, 1);
+    $expected_result = array(
+      'assignment' => "1",
+      'id' => "1",
+      'name' => "Problem1",
+      'score' => "100",
+      'is_upload_only' => "1",
+      'c_time_limit' => "500",
+      'python_time_limit' => "1500",
+      'java_time_limit' => "2000",
+      'memory_limit' => "50000",
+      'allowed_languages' => "C,C++,Python 2,Python 3,Java",
+      'diff_cmd' => "diff",
+      'diff_arg' => "-bB"
+    );
+    $test_name = "Test get problem info || input : - \nTime ~ Date: " . date('H:i:s ~ Y-m-d');
+    $this->unit->run($test, $expected_result, $test_name);
+  }
+
+  private function test_is_participant(){
+    $test = $this->Assignment_model->is_participant('ALL', 'example_username');
+    $expected_result = true;
+    $test_name = "Test is participant of all participants method in Assignment model || input : - \nTime ~ Date: " . date('H:i:s ~ Y-m-d');
+    $this->unit->run($test, $expected_result, $test_name);
+
+    $test = $this->Assignment_model->is_participant('ferdian, jeye, gopal, ivank, eririn', 'example_username');
+    $expected_result = false;
+    $test_name = "Test is participant of a subset participants method in Assignment model || input : - \nTime ~ Date: " . date('H:i:s ~ Y-m-d');
+    $this->unit->run($test, $expected_result, $test_name);
+
+    $test = $this->Assignment_model->is_participant('ferdian, jeye, gopal, ivank, eririn', 'jeye');
+    $expected_result = true;
+    $test_name = "Test is participant of a subset participants method in Assignment model || input : - \nTime ~ Date: " . date('H:i:s ~ Y-m-d');
+    $this->unit->run($test, $expected_result, $test_name);
+  }
+
+  private function test_save_problem_description(){
+    $desc = 'this is a problem desc';
+    $this->Assignment_model->save_problem_description(1, 1, $desc, 'html');
+    $this->Assignment_model->save_problem_description(1, 1, $desc, 'md');
+  }
+
+  private function add_dummy_submission() {
+    $this->db->query("INSERT INTO `shj_submissions` (`submit_id`, `username`, `assignment`, `problem`, `is_final`, `time`, `status`, `pre_score`, `coefficient`, `file_name`, `main_file_name`, `file_type`) VALUES ('1', 'test_username', '3', '1', '1', '2019-04-09 00:00:00', '', '0', '100', 'filenametxt', 'mainfilenametxt', 'txt')");
+  }
 }

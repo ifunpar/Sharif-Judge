124cc8ba0426a65cd8edde0f217cc150582de62a
Pascal Alfadian Nugroho <pascalalfadian@live.com>
2024-06-11 11:36:14 +0700

    Upgrade Twig to latest and last of 1.44.6

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
 /**
+ * SharIF Judge
+ *
+ * A free and open source online judge for programming assignments
+ *
+ * The web interface is written in PHP CodeIgniter framework.
+ *
+ */
+
+define('SHJ_VERSION','1.1-ifunpar');
+
+/**
+ * Cloudflare adaptor.
+ * This module will help the Framework know what actually happends
+ * on the client. This will manipulate the PHP's $_SERVER var for
+ * detecting IP, SCHEME, and other Usefull tools.
+ *
+*/
+if(isset($_SERVER['HTTP_CF_CONNECTING_IP']) && isset($_SERVER['HTTP_CF_RAY'])){
+	$_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
+	$_SERVER['HTTP_X_REAL_IP'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
+	if(isset($_SERVER['HTTP_CF_VISITOR']) || !isset($_SERVER['REQUEST_SCHEME'])){
+	  $_SERVER['REQUEST_SCHEME'] = (isset($_SERVER['HTTP_CF_VISITOR']) &&
+				   strpos($_SERVER['HTTP_CF_VISITOR'],'https')>-1)?'https':'http';
+	  $_SERVER['HTTPS'] = ($_SERVER['REQUEST_SCHEME']=="https")?"on":null;
+	}
+  }
+
+/**
  * CodeIgniter
  *
  * An open source application development framework for PHP
